511cf61cbb8b83b41aecb4851de15b1def4fc769
JhonoBrine <jhonpabroa@gmail.com>
2024-04-17 22:10:20 +0800

    Just Deleted UserDetailsEntity parameter

--- src/main/java/com/typecasters/apitowerofwords/Entity/UserItemEntity.java
+++ src/main/java/com/typecasters/apitowerofwords/Entity/UserItemEntity.java
@@ -18,18 +18,24 @@ public class UserItemEntity {
 //    @JoinColumn(name = "item_id")
     private int item_id;
 
-//    @JsonBackReference
-//    @ManyToOne
-//    @JoinColumn(name = "user_id")
-    private UserDetailsEntity user_details;
-
-    public UserItemEntity(int user_item_id, int quantity, int item_id, UserDetailsEntity user_details) {
+    public UserItemEntity(int user_item_id, int quantity, int item_id) {
         super();
         this.user_item_id = user_item_id;
         this.quantity = quantity;
         this.item_id = item_id;
-        this.user_details = user_details;
     }
+    //    @JsonBackReference
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private UserDetailsEntity user_details;
+
+//    public UserItemEntity(int user_item_id, int quantity, int item_id, UserDetailsEntity user_details) {
+//        super();
+//        this.user_item_id = user_item_id;
+//        this.quantity = quantity;
+//        this.item_id = item_id;
+//        this.user_details = user_details;
+//    }
 
     public UserItemEntity() {
         super();
@@ -59,11 +65,11 @@ public class UserItemEntity {
         this.item_id = item_id;
     }
 
-    public UserDetailsEntity getUser_details() {
-        return user_details;
-    }
-
-    public void setUser_details(UserDetailsEntity user_details) {
-        this.user_details = user_details;
-    }
+//    public UserDetailsEntity getUser_details() {
+//        return user_details;
+//    }
+//
+//    public void setUser_details(UserDetailsEntity user_details) {
+//        this.user_details = user_details;
+//    }
 }
