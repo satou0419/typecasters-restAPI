f2b7ded503b42780b0bd163431faa31ac67eca2f
CroseaMors <143801605+CroseaMors@users.noreply.github.com>
2024-05-02 01:18:45 +0800

    ADDED: Back btn, Floor Desc, and progress. LACKING: item pics and PROGRESS BAR(ALL STATIC)

--- src/AdventureMode.js
+++ src/AdventureMode.js
@@ -1,18 +1,51 @@
 import { Link } from "react-router-dom";
 import "./adventuremode.css";
-
+// import "./components/button.css";
 export default function AdventureMode() {
   return (
+    
     <main className="adventure-wrapper">
-      <section className="tower-container">
-        <Link to="/adventure_spelling">
-          <div className="floor-card">Floor 1</div>
-        </Link>
-        <div className="floor-card">Floor 2</div>
-        <div className="floor-card">Floor 3</div>
-        <div className="floor-card">Floor 4</div>
-        <div className="floor-card">Floor 5</div>
+      <button className="btn btn--large btn--danger--large btnBack">Back</button>
+     
+     {/* below is for the floor description */}
+      <section className="floor-description"> 
+
+        <div className="inner-box"></div>
+        <div className="desc-container">
+          <p className="txt-floor">Floor 1</p>
+          <p className="txt-rewards">Rewards:</p>
+
+          <div className="items-container">
+            <div className="reward-item">
+             <div className="reward-item-container"></div>
+            </div>
+            <div className="reward-item">
+              <div className="reward-item-container"></div>
+            </div>
+            <div className="reward-item">
+              <div className="reward-item-container"></div>
+            </div>
+          </div>  
+          <button className="btn btn--small btn--primary btn-enter">Enter</button>
+        </div>
       </section>
+      {/* end of floor description */}
+      <section className="tower-container">
+
+        <div className="floor-card">Floor 5</div>
+        <div className="floor-card">Floor 4</div>
+        <div className="floor-card">Floor 3</div>
+        <div className="floor-card">Floor 2</div>
+
+        <Link to="/adventure_spelling">
+        <div className="floor-card">Floor 1</div>
+        </Link>
+
+      </section>
+      
+      <div className="border-progress"></div>
+      <div className="progress">PROGRESS: </div>
+      <div className="progress-bar"></div>
     </main>
   );
 }
