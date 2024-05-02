539b115acecacbcd4735c36f29a3c2c469f4c2c3
JhonoBrine <jhonpabroa@gmail.com>
2024-05-02 15:58:14 +0800

    Changes in AdventureEnemyEntity, AdventureEnemyRepository.

--- src/main/java/com/typecasters/apitowerofwords/Repository/AdventureEnemyRepository.java
+++ src/main/java/com/typecasters/apitowerofwords/Repository/AdventureEnemyRepository.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.typecasters.apitowerofwords.Entity.AdventureEnemyEntity;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AdventureEnemyRepository extends JpaRepository<AdventureEnemyEntity, Integer> {
+    Optional<List<AdventureEnemyEntity>> findAllByTowerFloorId(int towerFloorId);
 }
