759def222ca39d4ce97b888469569b2c071faa46
CroseaMors <143801605+CroseaMors@users.noreply.github.com>
2024-05-09 19:21:11 +0800

    ADDED: Menu-icon(Animation)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./navigation.css";
-
 
 export default function Navigation({ onLogout }) {
   const [menuOpen, setMenuOpen] = useState(false);
   const [profileOpen, setProfileOpen] = useState(false);
-  const navigate = useNavigate();
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+    // setMenuOpen(!menuOpen);
+    setMenuOpen(prevMenuOpen => !prevMenuOpen);
   };
 
   const toggleProfile = () => {
@@ -19,17 +18,15 @@ export default function Navigation({ onLogout }) {
   return (
     <section>
       <nav className={`nav-bar ${menuOpen ? "menu-open" : ""}`}>
-      
         <div className="nav-bar__logo">
           <img src="./assets/logo/logo_simple.webp" alt="Logo" />
         </div>
-      
-        <div className="menu-icon" onClick={toggleMenu}>
+        <div className={`menu-icon ${menuOpen ? "active" : ""}`} onClick={toggleMenu}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
         </div>
-        <div className={`menu ${menuOpen ? "open" : "close"}`}>
+        <div className={`menu ${menuOpen ? "open" : ""}`}>
           <Link to="/adventure_mode">Adventure Mode</Link>
           <Link to="/simulation_mode">Simulation Mode</Link>
           <Link to="/archive">Archive</Link>
@@ -39,9 +36,9 @@ export default function Navigation({ onLogout }) {
         </div>
         <div className="profile-container">
           <div className="profile-icon" onClick={toggleProfile}>
-           <span className="circle"></span>
+            <span className="circle"></span>
           </div>
-          <div className={`profile ${profileOpen ? "open" : "close"}`}>
+          <div className={`profile ${profileOpen ? "open" : ""}`}>
             <Link onClick={onLogout}>Logout</Link>
           </div>
         </div>
