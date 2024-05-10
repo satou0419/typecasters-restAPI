a635599a149b6be6d24a724af15aefb2b0734ed0
JhonoBrine <jhonpabroa@gmail.com>
2024-05-10 11:02:48 +0800

    Added Http Response to UserDetailscController, and also added two new exceptions to be used for UserItemController and UserItemService

--- src/main/java/com/typecasters/apitowerofwords/Controller/UserDetailscController.java
+++ src/main/java/com/typecasters/apitowerofwords/Controller/UserDetailscController.java
@@ -3,6 +3,8 @@ package com.typecasters.apitowerofwords.Controller;
 import com.typecasters.apitowerofwords.Entity.UserDetailsEntity;
 import com.typecasters.apitowerofwords.Service.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,23 +15,30 @@ public class UserDetailscController {
     UserDetailsService ud_serv;
 
     @PutMapping("/update_user_credit/{ud_id}/{s_credit}")
-    public String updateUserCredit(@PathVariable int ud_id, @PathVariable int s_credit){
-
-        return ud_serv.updateUserCredit(ud_id,s_credit);
+    public ResponseEntity<String> updateUserCredit(@PathVariable int ud_id, @PathVariable int s_credit){
+        String result = ud_serv.updateUserCredit(ud_id, s_credit);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @PutMapping("/increment_word")
-    public String incrementUserWordCount(@RequestParam int ud_id){
-        return ud_serv.incrementUserDetailWords(ud_id);
+    public ResponseEntity<String> incrementUserWordCount(@RequestParam int ud_id){
+        String result = ud_serv.incrementUserDetailWords(ud_id);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @PutMapping("/increment_achievement")
-    public String incrementUserAchievementCount(@RequestParam int ud_id){
-        return ud_serv.incrementUserAchievementCount(ud_id);
+    public ResponseEntity<String> incrementUserAchievementCount(@RequestParam int ud_id){
+        String result = ud_serv.incrementUserAchievementCount(ud_id);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @GetMapping("/get_user_detail")
-    public UserDetailsEntity getUserDetails(@RequestParam int user_id){
-        return ud_serv.getUserDetails(user_id);
+    public ResponseEntity<UserDetailsEntity> getUserDetails(@RequestParam int user_id){
+        UserDetailsEntity userDetails = ud_serv.getUserDetails(user_id);
+        if (userDetails != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(userDetails);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
