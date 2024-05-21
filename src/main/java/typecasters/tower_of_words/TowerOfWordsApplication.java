f7b56f47bb4931cd6818f5970595ae7ff051a291
satou0419 <officialgarciareydate@gmail.com>
2024-05-21 16:43:47 +0800

    first commit

---
+++ src/main/java/typecasters/tower_of_words/TowerOfWordsApplication.java
@@ -0,0 +1,13 @@
+package typecasters.tower_of_words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowerOfWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TowerOfWordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/typecasters/tower_of_words/TowerOfWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package typecasters.tower_of_words;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TowerOfWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
