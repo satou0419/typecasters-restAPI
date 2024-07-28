af2d4959cd7f1981a6ee6729704e09da02275810
Acrylus <theantonjoseph@gmail.com>
2024-07-28 15:10:22 +0800

    User Update

--- src/main/java/typecasters/tower_of_words/Controller/UserController.java
+++ src/main/java/typecasters/tower_of_words/Controller/UserController.java
@@ -25,32 +25,12 @@ import java.util.NoSuchElementException;
 public class UserController {
 
     @Autowired
-    UserService userv;
-
-//    @PostMapping("/register")
-//    public ResponseEntity<String> registerUser(@RequestBody UserEntity user) {
-//        try {
-//            String registrationStatus = userv.registerUser(user);
-//            // Check if registration was successful
-//            if ("Registration Successful".equals(registrationStatus)) {
-//                return ResponseEntity.ok(registrationStatus);
-//            } else {
-//                // Registration failed, return error message
-//                return ResponseEntity.badRequest().body(registrationStatus);
-//            }
-//        } catch (IllegalArgumentException ex) {
-//            // Catch specific exception thrown by service layer and return error message
-//            return ResponseEntity.badRequest().body(ex.getMessage());
-//        } catch (Exception e) {
-//            // Handle unexpected exceptions
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during registration");
-//        }
-//    }
+    UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<Object> registerUser(@RequestBody UserEntity user) {
         try {
-            String registrationStatus = userv.registerUser(user);
+            String registrationStatus = userService.registerUser(user);
 
             if ("Registration Successful".equals(registrationStatus)) {
                 return NoDataResponse.noDataResponse(HttpStatus.OK, "Registration Successful");
@@ -65,23 +45,15 @@ public class UserController {
         }
     }
 
-
-
-
     @GetMapping("/user_test")
     public String testUser(){
         return "testinnnngg";
     }
 
-//    @GetMapping("/get_user_id/{username}")
-//    public int getUserId(@PathVariable String username){
-//        return userv.findUserIdByUsername(username);
-//    }
-//
     @GetMapping("/get_user_id/{username}")
     public ResponseEntity<Object> getUserId(@PathVariable String username){
         try{
-            int userId = userv.findUserIdByUsername(username);
+            int userId = userService.findUserIdByUsername(username);
             return Response.response(HttpStatus.OK, "User ID found", userId);
         }catch(NoSuchElementException e){
             return NoDataResponse.noDataResponse(HttpStatus.NOT_FOUND, e.getMessage());
@@ -90,27 +62,10 @@ public class UserController {
         }
     }
 
-//    @GetMapping("/get_user_info/{user_id}")
-//    public UserInfo getUserInfo(@PathVariable int user_id){
-//        return userv.findUserInfoById(user_id);
-//    }
-
-//    @GetMapping("/get_user_info/{user_id}")
-//    public ResponseEntity<Object> getUserInfo(@PathVariable int user_id){
-//        try{
-//            UserInfo userInfo = userv.findUserInfoById(user_id);
-//            return Response.response(HttpStatus.OK, "User Info found", userInfo);
-//        }catch(NoSuchElementException e){
-//            return NoDataResponse.noDataResponse(HttpStatus.BAD_REQUEST, e.getMessage());
-//        }catch(Exception e){
-//            return NoDataResponse.noDataResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-//        }
-//    }
-
     @PostMapping("/login")
     public ResponseEntity<Object> loginUser(@RequestBody LoginRequest logReq) {
         try {
-            LoginResponse loginResponse = userv.login(logReq);
+            LoginResponse loginResponse = userService.login(logReq);
             return Response.response(HttpStatus.OK, "Login successful", loginResponse); // 200 OK
         } catch (UsernameNotFoundException e) {
             return NoDataResponse.noDataResponse(HttpStatus.NOT_FOUND, e.getMessage()); // 404 Not Found
@@ -121,14 +76,10 @@ public class UserController {
         }
     }
 
-
-
-
-
-    @GetMapping("/get_user_info/{user_id}")
-    public ResponseEntity<Object> getUserInfo(@PathVariable int user_id) {
+    @GetMapping("/get_user_info/{userID}")
+    public ResponseEntity<Object> getUserInfo(@PathVariable int userID) {
         try {
-            UserInfoAndDetails userInfoAndDetails = userv.getUserInfo(user_id);
+            UserInfoAndDetails userInfoAndDetails = userService.getUserInfo(userID);
             return Response.response(HttpStatus.OK, "User info found", userInfoAndDetails);
         } catch (NoSuchElementException e) {
             return NoDataResponse.noDataResponse(HttpStatus.NOT_FOUND, "User not found");
@@ -138,9 +89,9 @@ public class UserController {
     }
 
     @PatchMapping("/change_password")
-    public ResponseEntity<Object> changePassword(@RequestParam int user_id, @RequestParam String oldPassword, @RequestParam String newPassword) {
+    public ResponseEntity<Object> changePassword(@RequestParam int userID, @RequestParam String oldPassword, @RequestParam String newPassword) {
         try {
-            boolean passwordChanged = userv.changePassword(user_id, oldPassword, newPassword);
+            boolean passwordChanged = userService.changePassword(userID, oldPassword, newPassword);
             if (passwordChanged) {
                 return NoDataResponse.noDataResponse(HttpStatus.OK, "Password changed successfully");
             } else {
@@ -155,11 +106,10 @@ public class UserController {
         }
     }
 
-
     @PatchMapping("/update_user")
-    public ResponseEntity<Object> updateUser(@RequestParam int uid, @RequestBody UserEntity newUserInfo) {
+    public ResponseEntity<Object> updateUser(@RequestParam int userID, @RequestBody UserEntity newUserInfo) {
         try {
-            userv.editAccount(newUserInfo, uid);
+            userService.editAccount(newUserInfo, userID);
             return NoDataResponse.noDataResponse(HttpStatus.OK, "User updated successfully");
         } catch (NoSuchElementException e) {
             return NoDataResponse.noDataResponse(HttpStatus.NOT_FOUND, "User not found");
@@ -168,23 +118,15 @@ public class UserController {
         }
     }
 
-
     @PostMapping("/test_find")
     public UserEntity loginUser(@RequestBody String username){
-        return userv.testFind(username);
+        return userService.testFind(username);
     }
 
-//    @RequestMapping(value = {"/logout"}, method = RequestMethod.POST)
-//    public String logoutUser(HttpServletRequest request, HttpServletResponse response, int user_id){
-//
-//        return "redirect:/login";
-//    }
-
     @RequestMapping(value = {"/logout"}, method = RequestMethod.PATCH)
-    public ResponseEntity<Object> logoutUser(@RequestParam int userId) {
+    public ResponseEntity<Object> logoutUser(@RequestParam int userID) {
         try {
-            userv.logout(userId);
-//            response.sendRedirect("/login");
+            userService.logout(userID);
             return NoDataResponse.noDataResponse(HttpStatus.OK, "User logged out successfully");
         } catch (LoggedOutException e){
             return NoDataResponse.noDataResponse(HttpStatus.CONFLICT, e.getMessage());
@@ -196,7 +138,7 @@ public class UserController {
     @GetMapping("/checkExist/{username}")
     public ResponseEntity<Object> checkUserExistence(@PathVariable String username) {
         try {
-            boolean userExists = userv.checkUserIfExist(username);
+            boolean userExists = userService.checkUserIfExist(username);
 
             if(userExists){
                 return Response.response(HttpStatus.OK, "User exists", userExists);
