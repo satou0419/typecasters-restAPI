3874969256f8c769f90fd142a8f98c3dee5a1461
Rey Dante Garcia <officialgarciareydante@gmail.com>
2024-08-26 20:25:50 +0800

    Server warmer added

--- src/main/java/typecasters/tower_of_words/TowerOfWordsApplication.java
+++ src/main/java/typecasters/tower_of_words/TowerOfWordsApplication.java
@@ -2,8 +2,11 @@ package typecasters.tower_of_words;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class TowerOfWordsApplication {
 
 	public static void main(String[] args) {
